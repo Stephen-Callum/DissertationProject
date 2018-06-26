4a54054822d554b7badb7e989da70f7ae3bf48bd
Stephen Callum <stephen.callum@hotmail.co.uk>
2018-06-26 22:38:09 +0100

    Player Movement scrpit cleaned up
    Added comments and made rigidbody public.

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -6,11 +6,11 @@ public class PlayerMovement : MonoBehaviour {
     public float speed;
     public float rotationSpeed;
 
-    Rigidbody2D rb2d;
+    public Rigidbody2D rb2dplayer;
 
 	// Use this for initialization
 	void Start () {
-        rb2d = GetComponent<Rigidbody2D>();
+        rb2dplayer = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
@@ -18,12 +18,19 @@ public class PlayerMovement : MonoBehaviour {
 		
 	}
 
+    // Rigidbody physics calculations every fixed framerate frame
     private void FixedUpdate()
     {
+        // Floating point variable for vertical input
         float thrust = Input.GetAxis("Vertical");
+        
+        // Floating point variable for horizontal input
         float rotate = Input.GetAxis("Horizontal");
 
-        rb2d.AddRelativeForce(Vector2.up * thrust * speed);
+        // Force added to relative y axis of rigidbody
+        rb2dplayer.AddRelativeForce(Vector2.up * thrust * speed);
+
+        // Rotate rigidbody on z axis
         transform.Rotate(0, 0, -rotate * rotationSpeed);
     }
 }
