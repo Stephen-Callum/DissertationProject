597e439e2009ec752424d0673cc494d85e817fb0
Stephen Callum <stephen.callum@hotmail.co.uk>
2018-06-26 21:55:42 +0100

    Added Player ship and movement.
    Rotation and forward thrust motion added.

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public float speed;
+    public float rotationSpeed;
+
+    Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        float thrust = Input.GetAxis("Vertical");
+        float rotate = Input.GetAxis("Horizontal");
+
+        rb2d.AddRelativeForce(Vector2.up * thrust * speed);
+        transform.Rotate(0, 0, -rotate * rotationSpeed);
+    }
+}
