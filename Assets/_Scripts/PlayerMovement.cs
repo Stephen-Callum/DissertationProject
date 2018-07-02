eb471397703b1a805448bfdf216c33ad6a18d2a6
Stephen Callum <stephen.callum@hotmail.co.uk>
2018-07-02 19:37:14 +0100

    Improvements to movement mechanics
    Manipulate transform.position on Update().

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -14,23 +14,39 @@ public class PlayerMovement : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        // Floating point variable for vertical input
+        float thrust = Input.GetAxis("Vertical");
+
+        // Floating point variable for horizontal input
+        float rotate = Input.GetAxis("Horizontal");
+
+        float distance = thrust * speed * Time.deltaTime;
+        Vector3 newPosition = new Vector3();
+        newPosition.x = transform.position.x + (distance * transform.up.x);
+        newPosition.y = transform.position.y + (distance * transform.up.y);
+        newPosition.z = transform.position.z + (distance * transform.up.z);
+
+        transform.position = newPosition;
+
+        // Rotate rigidbody on z axis
+        transform.Rotate(0, 0, -rotate * rotationSpeed);
+    }
 
     // Rigidbody physics calculations every fixed framerate frame
     private void FixedUpdate()
     {
-        // Floating point variable for vertical input
-        float thrust = Input.GetAxis("Vertical");
+        //// Floating point variable for vertical input
+        //float thrust = Input.GetAxis("Vertical");
         
-        // Floating point variable for horizontal input
-        float rotate = Input.GetAxis("Horizontal");
+        //// Floating point variable for horizontal input
+        //float rotate = Input.GetAxis("Horizontal");
 
-        // Force added to relative y axis of rigidbody
-        rb2dplayer.AddRelativeForce(Vector2.up * thrust * speed);
+        //// Force added to relative y axis of rigidbody
+        //rb2dplayer.AddForce(transform.up * thrust * speed);
 
-        // Rotate rigidbody on z axis
-        transform.Rotate(0, 0, -rotate * rotationSpeed);
+        //// Rotate rigidbody on z axis
+        //transform.Rotate(0, 0, -rotate * rotationSpeed);
     }
 }
