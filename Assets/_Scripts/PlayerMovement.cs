7568c4c199099d014d4b8de55f4781bf4f560b07
Stephen Callum <stephen.callum@hotmail.co.uk>
2018-07-02 19:44:46 +0100

    Testing Screen Wrapping function.
    Teleport player to opposite side of screen when they reach a certain thresh-hold

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -5,26 +5,33 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
     public float speed;
     public float rotationSpeed;
+    public float topOfScreen;
+    public float bottomOfScreen;
+    public float leftOfScreen;
+    public float rightOfScreen;
 
     public Rigidbody2D rb2dplayer;
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         rb2dplayer = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        Debug.Log(transform.position);
+        ScreenWrap();
 	}
 
     // Rigidbody physics calculations every fixed framerate frame
     private void FixedUpdate()
     {
-        // Floating point variable for vertical input
+        // Check for vertical input from keyboard
         float thrust = Input.GetAxis("Vertical");
         
-        // Floating point variable for horizontal input
+        // Check for horizontal input from keyboard
         float rotate = Input.GetAxis("Horizontal");
 
         // Force added to relative y axis of rigidbody
@@ -32,5 +39,27 @@ public class PlayerMovement : MonoBehaviour {
 
         // Rotate rigidbody on z axis
         transform.Rotate(0, 0, -rotate * rotationSpeed);
+    }
+    
+    // Handle Screen wrapping
+    void ScreenWrap()
+    {
+        Vector2 newPosition = transform.position;
+        if (transform.position.y > topOfScreen)
+        {
+            newPosition.y = bottomOfScreen;
+        }
+        if (transform.position.y < bottomOfScreen)
+        {
+            newPosition.y = topOfScreen;
+        }
+        if (transform.position.x > rightOfScreen)
+        {
+            newPosition.x = leftOfScreen;
+        }
+        if (transform.position.x < leftOfScreen)
+        {
+            newPosition.x = rightOfScreen;
+        }
     }
 }
