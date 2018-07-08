df23489a23c29e9d993d6ce083e2da28fe01353a
Stephen Callum <stephen.callum@hotmail.co.uk>
2018-07-09 00:04:00 +0100

    Added Enemy ship
    Elementary bullet physics included

---
+++ Assets/_Scripts/BulletMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour {
+    public float bulletSpeed;
+
+    public Rigidbody2D bulletRB;
+	
+    // Use this for initialization
+	void Start () {
+        bulletRB = GetComponent<Rigidbody2D>();
+        bulletRB.AddForce(transform.up * bulletSpeed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
